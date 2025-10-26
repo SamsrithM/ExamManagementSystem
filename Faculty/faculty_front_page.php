c73c3e7aa1427946a47e89f531e79ed644aee482
Irfan <mahammadirfan4242@gmail.com>
2025-10-26 09:23:30 +0530

    Update faculty_front_page.php

--- Faculty/faculty_front_page.php
+++ Faculty/faculty_front_page.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Database connection using environment variables
-$db_host = getenv('DB_HOST') ?: 'localhost';
+$db_host = getenv('DB_HOST') ?: '127.0.0.1';
 $db_user = getenv('DB_USER') ?: 'root';
 $db_pass = getenv('DB_PASS') ?: '';
 $db_name = getenv('DB_NAME') ?: 'new_registration_data';

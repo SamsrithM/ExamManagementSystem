8a50dcc84f0e0a23963e41a740036c1b920d1d0d
Irfan <mahammadirfan4242@gmail.com>
2025-10-26 09:46:04 +0530

    Update free_slots.php

--- Admin/free_slots.php
+++ Admin/free_slots.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION['admin_user'])) {
 $db_host = getenv('DB_HOST') ?: '127.0.0.1';
 $db_user = getenv('DB_USER') ?: 'root';
 $db_pass = getenv('DB_PASS') ?: '';
-$db_name = getenv('DB_NAME') ?: 'room_allocation';
+$db_name = getenv('DB_ROOM') ?: 'room_allocation';
 
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 if ($conn->connect_error) {

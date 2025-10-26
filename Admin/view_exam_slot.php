d52caebf53baf81a4be9afdfc192235580b00d8b
Irfan <mahammadirfan4242@gmail.com>
2025-10-26 09:21:38 +0530

    Update view_exam_slot.php

--- Admin/view_exam_slot.php
+++ Admin/view_exam_slot.php
@@ -2,7 +2,7 @@
 // view_slots.php
 
 // Database connection using environment variables
-$db_host = getenv('DB_HOST') ?: 'localhost';
+$db_host = getenv('DB_HOST') ?: '127.0.0.1';
 $db_user = getenv('DB_USER') ?: 'root';
 $db_pass = getenv('DB_PASS') ?: '';
 $db_name = getenv('DB_NAME') ?: 'room_allocation';

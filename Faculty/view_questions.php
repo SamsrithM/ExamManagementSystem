e86e2bd77493825cfaa6b0b346955141474bf5e7
Irfan <mahammadirfan4242@gmail.com>
2025-10-26 10:00:29 +0530

    Update view_questions.php

--- Faculty/view_questions.php
+++ Faculty/view_questions.php
@@ -5,7 +5,7 @@ session_start();
 $db_host = getenv('DB_HOST') ?: '127.0.0.1';
 $db_user = getenv('DB_USER') ?: 'root';
 $db_pass = getenv('DB_PASS') ?: '';
-$db_name = getenv('DB_NAME') ?: 'test_creation';
+$db_name = getenv('DB_TEST') ?: 'test_creation';
 
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 if ($conn->connect_error) {

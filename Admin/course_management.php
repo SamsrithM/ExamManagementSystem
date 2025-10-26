abdf918ffaeab939075f30c86fbf021b0341a9e3
Irfan <mahammadirfan4242@gmail.com>
2025-10-26 10:25:24 +0530

    Update course_management.php

--- Admin/course_management.php
+++ Admin/course_management.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['admin_user'])) {
 }
 
 // Database connection (render-ready using env variables)
-$db_host = getenv('DB_HOST') ?: '127.0.0.1';
+$db_host = getenv('DB_HOST') ?: 'mysql';
 $db_user = getenv('DB_USER') ?: 'root';
 $db_pass = getenv('DB_PASS') ?: '';
 $db_name = getenv('DB_COURSE') ?: 'course_registration_data';

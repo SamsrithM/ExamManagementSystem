bed61646e0a1d5505b4a65dfb3f3deebae6842f4
Irfan <mahammadirfan4242@gmail.com>
2025-10-26 09:29:14 +0530

    Update submit_exam.php

--- Student/submit_exam.php
+++ Student/submit_exam.php
@@ -25,7 +25,7 @@ if ($test_id <= 0) {
 }
 
 // --- DB Connection using environment variables ---
-$db_host = getenv('DB_HOST') ?: 'localhost';
+$db_host = getenv('DB_HOST') ?: '127.0.0.1';
 $db_user = getenv('DB_USER') ?: 'root';
 $db_pass = getenv('DB_PASS') ?: '';
 $db_name = getenv('TEST_DB') ?: 'test_creation';

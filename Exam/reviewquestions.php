dc32f8df5bbbdbc755e05e1eb342ec955eae4338
Irfan <mahammadirfan4242@gmail.com>
2025-10-26 10:27:28 +0530

    Update reviewquestions.php

--- Exam/reviewquestions.php
+++ Exam/reviewquestions.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['faculty_user'])) {
 }
 
 // Database connection using environment variables
-$db_host = getenv('DB_HOST') ?: '127.0.0.1';
+$db_host = getenv('DB_HOST') ?: 'mysql';
 $db_user = getenv('DB_USER') ?: 'root';
 $db_pass = getenv('DB_PASS') ?: '';
 $db_name = getenv('DB_TEST') ?: 'test_creation';

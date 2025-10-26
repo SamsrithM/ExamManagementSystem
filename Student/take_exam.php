e33e9bec4c571b396147d391b8f3793874efd623
Irfan <mahammadirfan4242@gmail.com>
2025-10-26 09:29:00 +0530

    Update take_exam.php

--- Student/take_exam.php
+++ Student/take_exam.php
@@ -13,7 +13,7 @@ if ($test_id <= 0) {
 }
 
 // --- DB connection using environment variables ---
-$db_host = getenv('DB_HOST') ?: 'localhost';
+$db_host = getenv('DB_HOST') ?: '127.0.0.1';
 $db_user = getenv('DB_USER') ?: 'root';
 $db_pass = getenv('DB_PASS') ?: '';
 $db_name = getenv('DB_NAME') ?: 'test_creation';

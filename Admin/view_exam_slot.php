d86507f0051df1cdc18b6999c635af0197a1056a
Irfan <mahammadirfan4242@gmail.com>
2025-10-26 09:46:40 +0530

    Update view_exam_slot.php

--- Admin/view_exam_slot.php
+++ Admin/view_exam_slot.php
@@ -5,7 +5,7 @@
 $db_host = getenv('DB_HOST') ?: '127.0.0.1';
 $db_user = getenv('DB_USER') ?: 'root';
 $db_pass = getenv('DB_PASS') ?: '';
-$db_name = getenv('DB_NAME') ?: 'room_allocation';
+$db_name = getenv('DB_ROOM') ?: 'room_allocation';
 
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 

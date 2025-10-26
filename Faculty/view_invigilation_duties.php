49779114d80d31debda16a29c5c3a10392761dc0
Irfan <mahammadirfan4242@gmail.com>
2025-10-26 09:25:00 +0530

    Update view_invigilation_duties.php

--- Faculty/view_invigilation_duties.php
+++ Faculty/view_invigilation_duties.php
@@ -9,7 +9,7 @@ if (!isset($_SESSION['faculty_user'])) {
 $faculty_email = $_SESSION['faculty_user'];
 
 // Database connection using environment variables
-$db_host = getenv('DB_HOST') ?: 'localhost';
+$db_host = getenv('DB_HOST') ?: '127.0.0.1';
 $db_user = getenv('DB_USER') ?: 'root';
 $db_pass = getenv('DB_PASS') ?: '';
 $db_name = getenv('DB_NAME') ?: 'room_allocation';

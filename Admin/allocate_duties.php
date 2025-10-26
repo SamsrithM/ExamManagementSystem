b80b65c4ed3638488099fbe02959f00095111322
Irfan <mahammadirfan4242@gmail.com>
2025-10-26 10:24:58 +0530

    Update allocate_duties.php

--- Admin/allocate_duties.php
+++ Admin/allocate_duties.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Render-ready environment DB connection
-$db_host = getenv('DB_HOST') ?: '127.0.0.1';
+$db_host = getenv('DB_HOST') ?: 'mysql';
 $db_user = getenv('DB_USER') ?: 'root';
 $db_pass = getenv('DB_PASS') ?: '';
 $db_name = getenv('DB_ROOM') ?: 'room_allocation';

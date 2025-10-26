3c919792a136ccbe706246490150b2584f9201f7
Irfan <mahammadirfan4242@gmail.com>
2025-10-26 09:45:11 +0530

    Update course_management_student.php

--- Admin/course_management_student.php
+++ Admin/course_management_student.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION['student_user'])) {
 $db_host = getenv('DB_HOST') ?: '127.0.0.1';
 $db_user = getenv('DB_USER') ?: 'root';
 $db_pass = getenv('DB_PASS') ?: '';
-$db_name = getenv('DB_NAME') ?: 'course_management';
+$db_name = getenv('DB_MANAGE') ?: 'course_management';
 
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 if ($conn->connect_error) {

a264cb5b909db7a5f6dc6c56c45acdd4eeae9241
Irfan <mahammadirfan4242@gmail.com>
2025-10-26 09:21:22 +0530

    Change database host from localhost to 127.0.0.1

--- Admin/student_list_admin.php
+++ Admin/student_list_admin.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['admin_user'])) {
 }
 
 // Database connection using environment variables
-$db_host = getenv('DB_HOST') ?: 'localhost';
+$db_host = getenv('DB_HOST') ?: '127.0.0.1';
 $db_user = getenv('DB_USER') ?: 'root';
 $db_pass = getenv('DB_PASS') ?: '';
 $db_name = getenv('DB_NAME') ?: 'new_registration_data';

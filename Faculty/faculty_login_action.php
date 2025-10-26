75589a667a757286184efbe682dbf2c3abbc1dd1
Irfan <mahammadirfan4242@gmail.com>
2025-10-26 09:23:46 +0530

    Update faculty_login_action.php

--- Faculty/faculty_login_action.php
+++ Faculty/faculty_login_action.php
@@ -2,7 +2,7 @@
 session_start();
 
 // DB connection using environment variables
-$db_host = getenv('DB_HOST') ?: 'localhost';
+$db_host = getenv('DB_HOST') ?: '127.0.0.1';
 $db_user = getenv('DB_USER') ?: 'root';
 $db_pass = getenv('DB_PASS') ?: '';
 $db_name = getenv('FACULTY_DB') ?: 'faculty_data';

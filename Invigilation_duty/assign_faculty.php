5bd9682e3977c5eec6d7f29cbf80abc607adfdea
Irfan <mahammadirfan4242@gmail.com>
2025-10-26 09:26:10 +0530

    Update assign_faculty.php

--- Invigilation_duty/assign_faculty.php
+++ Invigilation_duty/assign_faculty.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/json');
 
 // Use environment variables for deployment
-$host = getenv('DB_HOST') ?: 'localhost';
+$host = getenv('DB_HOST') ?: '127.0.0.1';
 $user = getenv('DB_USER') ?: 'root';
 $pass = getenv('DB_PASS') ?: '';
 $db_name = getenv('ROOM_DB') ?: 'room_allocation';
